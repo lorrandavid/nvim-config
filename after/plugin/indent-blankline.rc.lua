4abcc6b499a0eb397c319448e419438cc4e21a12
Lorran David <lorrantd1@gmail.com>
2022-09-22 23:17:54 -0300

    Indent blankline char & colors

--- after/plugin/indent-blankline.rc.lua
+++ after/plugin/indent-blankline.rc.lua
@@ -1,4 +1,20 @@
 local status, indent_blankline = pcall(require, 'indent_blankline')
 if (not status) then return end
 
-indent_blankline.setup {}
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.termguicolors = true
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#454545 gui=nocombine]]
+
+indent_blankline.setup {
+  char = "¦",
+  space_char_blankline = " ",
+  space_char_highlight_list = {
+    "IndentBlanklineIndent1"
+  },
+  show_current_context = true,
+  show_current_context_start = true,
+  char_highlight_list = {
+    "IndentBlanklineIndent1"
+  }
+}
