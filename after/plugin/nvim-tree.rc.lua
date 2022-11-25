57aa240ad14b561bbc75e0f9e33072699b329d71
Lorran David <lorrantd1@gmail.com>
2022-11-25 02:10:17 -0300

    Change nvim tree to float mode

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -2,14 +2,28 @@ local status, nvim_tree = pcall(require, 'nvim-tree')
 if (not status) then return end
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+local gwidth = vim.api.nvim_list_uis()[1].width
+local gheight = vim.api.nvim_list_uis()[1].height
+local width = 100
+local height = 30
 
 nvim_tree.setup({
   hijack_cursor = false,
   hijack_netrw = false,
   open_on_setup = true,
   view = {
+    float = {
+      enable = true,
+      open_win_config = {
+        relative = "editor",
+        width = width,
+        height = height,
+        row = (gheight - height) * 0.4,
+        col = (gwidth - width) * 0.5,
+      },
+    },
+    -- adaptive_size = true,
     side = "left",
-    width = 30,
     mappings = {
       list = {
         { key = 'L', cb = tree_cb('cd') },
